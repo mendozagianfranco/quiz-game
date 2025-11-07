f6c2ce146343a1409fd30e7bbc686216a49bc7ce
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-11-07 16:29:18 +0100

    feat: add simple structure html homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
     return (
-        <></>
+        <>
+            <div>
+                <div>
+                    <h1>Quiz Game</h1>
+                    <p> Metti alla prova le tue conoscenze!
+                        Rispondi a una serie di domande e scopri il tuo punteggio finale.
+                    </p>
+                    <div>
+                        <select >
+                            <option>Facile</option>
+                            <option>Medio</option>
+                            <option>Difficile</option>
+                        </select>
+                    </div>
+                    <Link to={'/quiz'}>Inizia il Quiz</Link>
+                </div>
+            </div>
+        </>
     );
 }
