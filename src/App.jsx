3fd3a3124d873dd3880478c2b17832ecaee07a2b
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-11-07 13:37:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/quiz' element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
