b2706b12f01637a5e40fea1a94e23f5bac09ee93
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-11-07 17:36:59 +0100

    style: add style to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,24 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return (
         <>
-            <div>
-                <div>
-                    <h1>Quiz Game</h1>
-                    <p> Metti alla prova le tue conoscenze!
+            <div className='flex flex-col justify-center items-center text-white text-center bg-linear-to-br from-blue-500 to-indigo-700  h-screen '>
+                <div className='px-6' >
+                    <h1 className='text-5xl font-bold mb-4'>Quiz Game</h1>
+                    <p className='text-lg mb-8 text-gray-100'> Metti alla prova le tue conoscenze!
                         Rispondi a una serie di domande e scopri il tuo punteggio finale.
                     </p>
-                    <div>
-                        <select >
+
+                    <div className='flex flex-col sm:flex-row gap-4 mb-6 justify-center'>
+                        <select className="px-4 py-2 rounded-lg border-2 border-white bg-white text-blue-600 font-semibold shadow-md transition">
                             <option>Facile</option>
                             <option>Medio</option>
                             <option>Difficile</option>
                         </select>
                     </div>
-                    <Link to={'/quiz'}>Inizia il Quiz</Link>
+                    <Link
+                        to={'/quiz'}
+                        className='bg-white text-blue-600 font-semibold px-6 py-3 rounded-lg hover:bg-gray-300 transition'
+                    >Inizia il Quiz</Link>
                 </div>
             </div>
         </>
