38544b64f415fd6b444efddca5fad972d00fe01f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-11-10 17:24:08 +0100

    feat: add logic button with select difficultuy and save in context in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { useQuizContext } from '../hooks/useQuizContext';
 
 export default function Home() {
+    const difficultyRef = useRef();
+    const { setDifficulty } = useQuizContext();
+
+    function handleStartQuiz() {
+        setDifficulty(difficultyRef.current.value);
+    }
+
     return (
         <>
             <div className='flex flex-col justify-center items-center text-white text-center bg-linear-to-br from-blue-600 via-blue-700 to-indigo-800  h-screen '>
@@ -11,15 +20,16 @@ export default function Home() {
                     </p>
 
                     <div className='flex flex-col sm:flex-row gap-4 mb-6 justify-center'>
-                        <select className="px-4 py-2 rounded-lg border-2 border-white bg-white text-blue-600 font-semibold shadow-md transition">
-                            <option>Facile</option>
-                            <option>Medio</option>
-                            <option>Difficile</option>
+                        <select ref={difficultyRef} className="px-4 py-2 rounded-lg border-2 border-white bg-white text-blue-600 font-semibold shadow-md transition">
+                            <option value={'easy'}>Facile</option>
+                            <option value={'medium'}>Medio</option>
+                            <option value={'hard'}>Difficile</option>
                         </select>
                     </div>
                     <Link
                         to={'/quiz'}
                         className='bg-white text-blue-600 font-semibold px-6 py-3 rounded-lg hover:bg-gray-300 transition'
+                        onClick={handleStartQuiz}
                     >Inizia il Quiz</Link>
                 </div>
             </div>
