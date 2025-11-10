20c208404e9784d6f5618e860a5e4a36a06695f3
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-11-10 17:23:03 +0100

    feat: add QuizProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
+import { QuizProvider } from './context/GlobalContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/quiz' element={<Quiz />} />
-        </Routes>
-      </BrowserRouter>
+      <QuizProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/quiz' element={<Quiz />} />
+          </Routes>
+        </BrowserRouter>
+      </QuizProvider>
     </>
   );
 }
