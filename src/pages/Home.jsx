59104682f4323196fe685efe455bc1b7ac41bd0f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-11-11 16:08:30 +0100

    fix: file path useQuizContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { useQuizContext } from '../hooks/useQuizContext';
+import { useQuizContext } from '../context/useQuizContext';
 
 export default function Home() {
     const difficultyRef = useRef();
