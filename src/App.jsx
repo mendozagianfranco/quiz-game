fba752f02ba9cffcd5512a818bdb71b036a9d7cf
mendozagianfranco <mendozagianfranco88@gmail.com>
2026-02-18 12:39:43 +0100

    feat: add new Route for Score

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
+import Score from './pages/Score';
 import { QuizProvider } from './context/GlobalContext';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/quiz' element={<Quiz />} />
+            <Route path='/score' element={<Score />} />
           </Routes>
         </BrowserRouter>
       </QuizProvider>
