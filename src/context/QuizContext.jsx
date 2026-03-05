ac452b49a5d5252ce80096f3ca80859a1d400383
mendozagianfranco <mendozagianfranco88@gmail.com>
2026-03-05 23:43:49 +0100

    feat: change name GloblaContext in QuizContext and separate logics

---
+++ src/context/QuizContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from 'react';
+import { useQuizGame } from '../hooks/useQuizGame';
+
+const QuizContext = createContext();
+
+function QuizProvider({ children }) {
+    const quiz = useQuizGame();
+
+    return (
+        <QuizContext.Provider value={{ quiz }}>
+            {children}
+        </QuizContext.Provider>
+    );
+}
+
+
+export { QuizProvider, QuizContext };
