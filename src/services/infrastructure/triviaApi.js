bdc417b54419ab9f85c6de27985c0e829537f012
mendozagianfranco <mendozagianfranco88@gmail.com>
2026-03-12 22:47:19 +0100

    feat: add try catch for fecthQuestions

--- src/services/infrastructure/triviaApi.js
+++ src/services/infrastructure/triviaApi.js
@@ -2,8 +2,16 @@ import { transformQuestions } from '../domain/transformQuestions';
 
 export async function fetchQuestions(difficulty, total = 10) {
 
-    const res = await fetch(`https://opentdb.com/api.php?amount=${total}&difficulty=${difficulty}&type=multiple`);
-    const data = await res.json();
-    if (!data.results) return [];
-    return transformQuestions(data.results);
+    try {
+        const res = await fetch(`https://opentdb.com/api.php?amount=${total}&difficulty=${difficulty}&type=multiple`);
+        if (!res.ok) {
+            throw new Error('Errore chiamate API');
+        }
+        const data = await res.json();
+        if (!data.results) return [];
+        return transformQuestions(data.results);
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
